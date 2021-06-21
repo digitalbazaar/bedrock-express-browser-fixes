2359bb224b62b2c0040478e885dbc72ed428fd99
Josh Dunn <jdunn@digitalbazaar.com>
2021-06-21 11:02:23 -0600

    Update tests.

--- test/10-api.js
+++ test/10-api.js
@@ -1,13 +1,57 @@
 /*
  * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
+import bedrock from 'bedrock';
+const config = bedrock.config;
 import {safariSameSiteFix} from 'bedrock-express-browser-fixes';
-import request from 'supertest';
+// import request from 'supertest';
+import request from 'request';
 import http from 'http';
+// import 'bedrock-express-browser-fixes';
 
 const mockNext = function() {};
 
 describe('SameSite Tests', () => {
+  it.only('Successfully updates SameSite for Safari 14.0.0', done => {
+    console.log(config.server.baseUri + '/test');
+    request
+      .defaults({strictSSL: false})
+      .get({url: config.server.baseUri + '/test'})
+      .on('response', function(response) {
+        console.log(response.statusCode); // 200
+        console.log(response.headers['content-type']); // 'image/png'
+      });
+    done();
+  });
+
+  it('Successfully updates SameSite for Safari 14.0.0', done => {
+
+    const server = createServer(handler);
+
+    function handler(req, res) {
+      res.setHeader('Set-Cookie', [
+        '_csrf=S2deWMA5zpIdea2yQljsHCdh; Path=/; Secure; SameSite=Strict',
+        'sid=s%3AjqFd8RlkzPVvjBAbY6U4k9P6_FW31NLP.JW6Zpp7Rnrbxbp' +
+        'rjS%2FvvIp%2BLshX4UbXHvIMaCRv1IJA; Path=/; HttpOnly; Secure; ' +
+        'SameSite=None'
+      ]);
+      safariSameSiteFix(req, res, mockNext);
+    }
+
+    request(server)
+      .get('/')
+      .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) ' +
+        'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 ' +
+        'Safari/605.1.15')
+      .expect(res => {
+        res.headers['set-cookie'] = [
+          '_csrf=S2deWMA5zpIdea2yQljsHCdh; Path=/; Secure; SameSite=Strict',
+          'sid=s%3AjqFd8RlkzPVvjBAbY6U4k9P6_FW31NLP.JW6Zpp7Rnrb' +
+          'xbprjS%2FvvIp%2BLshX4UbXHvIMaCRv1IJA; Path=/; HttpOnly; Secure;'
+        ];
+      })
+      .expect(200, done);
+  });
   it('Successfully updates SameSite for Safari 14.0.0', done => {
 
     const server = createServer(handler);
