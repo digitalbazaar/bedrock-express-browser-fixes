064d11a1050ad1be049941f5158b1177ee3b7c57
Josh Dunn <jdunn@digitalbazaar.com>
2021-06-22 09:22:18 -0600

    Update file structure and add use strict to test.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
+'use strict';
+
 const bedrock = require('bedrock');
 const config = bedrock.config;
 const https = require('https');
